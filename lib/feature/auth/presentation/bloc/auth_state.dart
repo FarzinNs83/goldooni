90c53225a7e94ce0c990aa3c9430f90ff967fda6
FarzinNs83 <farzinns83@gmail.com>
2025-12-03 15:00:00 +0330

    refactor(auth): refine AuthState

--- lib/feature/auth/presentation/bloc/auth_state.dart
+++ lib/feature/auth/presentation/bloc/auth_state.dart
@@ -3,3 +3,40 @@ part of 'auth_bloc.dart';
 abstract class AuthState {}
 
 class AuthInitial extends AuthState {}
+
+class AuthLoading extends AuthState {}
+
+class AuthError extends AuthState {
+  final Failure failure;
+  AuthError(this.failure);
+}
+
+class AuthTimerTick extends AuthState {
+  final int time;
+  AuthTimerTick(this.time);
+}
+
+class AuthTimerFinished extends AuthState {
+  final String message;
+  AuthTimerFinished(this.message);
+}
+
+class AuthSendSms extends AuthState {
+  final SendSmsEntity sendSmsEntity;
+
+  AuthSendSms({required this.sendSmsEntity});
+}
+
+class AuthVerifyOtp extends AuthState {
+  final VerifyOtpEntity verifyOtpEntity;
+  AuthVerifyOtp({required this.verifyOtpEntity});
+}
+
+class AuthRegister extends AuthState {
+  final RegisterFormEntity registerFormEntity;
+  AuthRegister({required this.registerFormEntity});
+}
+
+class AuthIsRegistered extends AuthState {}
+
+class AuthIsNotRegistered extends AuthState {}
