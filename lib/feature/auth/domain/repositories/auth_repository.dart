c28748f694b3d998a2b527b778c475eb49061d53
FarzinNs83 <farzinns83@gmail.com>
2025-12-03 14:59:59 +0330

    refactor(auth): update AuthRepository interface

--- lib/feature/auth/domain/repositories/auth_repository.dart
+++ lib/feature/auth/domain/repositories/auth_repository.dart
@@ -1,1 +1,12 @@
-abstract class AuthRepository {}
+import 'package:fpdart/fpdart.dart';
+import 'package:goldooni/feature/auth/domain/entities/register_form_entity.dart';
+import 'package:goldooni/feature/auth/domain/entities/send_sms_entity.dart';
+import 'package:goldooni/feature/auth/domain/entities/verify_otp_entity.dart';
+
+import '../../../../core/errors/failure.dart';
+
+abstract class AuthRepository {
+  Future<Either<Failure,SendSmsEntity>> sendSmsOtp(SendSmsEntity sendSms);
+  Future<Either<Failure,VerifyOtpEntity>> verifyOtp(VerifyOtpEntity verifyOtp);
+  Future<Either<Failure,RegisterFormEntity>> registerForm(RegisterFormEntity registerForm);
+}
