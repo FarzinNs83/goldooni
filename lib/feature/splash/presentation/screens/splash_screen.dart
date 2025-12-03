eae6a426fc914a6a0b121f3220b6cfdd5978d8d7
FarzinNs83 <farzinns83@gmail.com>
2025-12-03 15:00:14 +0330

    feat(splash): update SplashScreen

--- lib/feature/splash/presentation/screens/splash_screen.dart
+++ lib/feature/splash/presentation/screens/splash_screen.dart
@@ -1,36 +1,86 @@
+
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:goldooni/core/utils/app_ext.dart';
+import 'package:goldooni/core/utils/shared_pref_manager.dart' show SharedPref;
+import 'package:goldooni/feature/auth/presentation/screens/send_sms_screen.dart';
+import 'package:goldooni/feature/splash/presentation/bloc/splash_bloc.dart';
 import 'package:goldooni/gen/assets.gen.dart';
+import 'package:goldooni/main_wrapper.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 
-import '../../../../core/resources/app_colors.dart';
-
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    final token = SharedPref.instance.getString('token');
+    final ref = SharedPref.instance.getString('refresh');
+    log(token.toString());
+    log(ref.toString());
+
+    if (token != null) {
+      context.read<SplashBloc>().validateAccessToken();
+    } else {
+      Future.microtask(() {
+        if (mounted) context.navigateR(SendSmsScreen());
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final colors = context.theme.colorScheme;
+
     return Scaffold(
-      backgroundColor: AppColors.primaryColor,
-      body: Stack(
-        children: [
-          Center(
-            child: SvgPicture.asset(
-              Assets.svg.logo,
-              height: context.screenHeight * .1,
+      backgroundColor: colors.primary,
+      body: BlocListener<SplashBloc, SplashState>(
+        listener: (context, state) {
+          if (state is SplashAccessSuccess) {
+            context.navigateR(MainWrapper());
+          }
+
+          if (state is SplashRefreshFailure) {
+            context.navigateR(SendSmsScreen());
+          }
+        },
+        child: Stack(
+          children: [
+            Center(
+              child: SvgPicture.asset(
+                Assets.svg.logo,
+                height: 0.1.sh,
+                colorFilter: ColorFilter.mode(
+                  colors.onPrimary,
+                  BlendMode.srcIn,
+                ),
+              ),
             ),
-          ),
-          Positioned(
-            bottom: 24,
-            left: 0,
-            right: 0,
-            child: LoadingAnimationWidget.dotsTriangle(
-              color: AppColors.white,
-              size: context.screenHeight * .06,
+            Positioned(
+              bottom: 24.h,
+              left: 0,
+              right: 0,
+              child: Directionality(
+                textDirection: TextDirection.ltr,
+                child: LoadingAnimationWidget.halfTriangleDot(
+                  color: colors.onPrimary,
+                  size: 0.06.sh,
+                ),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
