12fc31b68b56f71677dab92a5373477ca691af6f
FarzinNs83 <farzinns83@gmail.com>
2025-12-03 15:00:18 +0330

    refactor: update app bootstrap

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:goldooni/core/resources/app_colors.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:goldooni/core/resources/app_theme.dart';
-import 'package:goldooni/feature/auth/presentation/screens/register_form_screen.dart';
-import 'package:goldooni/feature/auth/presentation/screens/send_sms_screen.dart';
-import 'package:goldooni/feature/auth/presentation/screens/verify_otp_screen.dart';
+import 'package:goldooni/feature/auth/presentation/bloc/auth_bloc.dart';
+import 'package:goldooni/feature/home/presentation/bloc/home_bloc.dart';
+import 'package:goldooni/feature/profile/presentation/bloc/profile_bloc.dart';
+import 'package:goldooni/feature/singleproduct/presentation/bloc/single_product_bloc.dart';
+import 'package:goldooni/feature/splash/presentation/bloc/splash_bloc.dart';
 import 'package:goldooni/feature/splash/presentation/screens/splash_screen.dart';
+import 'di_container.dart' as di;
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      statusBarIconBrightness: Brightness.dark,
-      systemNavigationBarColor: AppColors.scaffoldBackgroundColor,
-      systemNavigationBarIconBrightness: Brightness.dark,
-    ),
-  );
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  SystemChrome.setSystemUIOverlayStyle(AppTheme.lightOverlay);
+  await di.init();
   runApp(const MainApp());
 }
 
@@ -26,17 +25,45 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      localizationsDelegates: [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: [Locale('fa', 'IR')],
-      locale: Locale('fa', 'IR'),
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.appTheme,
-      home: SendSmsScreen(),
+    return ScreenUtilInit(
+      designSize: Size(390, 844),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, _) => MultiBlocProvider(
+        providers: [
+          BlocProvider<AuthBloc>(create: (context) => di.sl<AuthBloc>()),
+          BlocProvider<HomeBloc>(create: (context) => di.sl<HomeBloc>()),
+          BlocProvider<ProfileBloc>(create: (context) => di.sl<ProfileBloc>()),
+          BlocProvider<SplashBloc>(create: (context) => di.sl<SplashBloc>()),
+          BlocProvider<SingleProductBloc>(create: (context) => di.sl<SingleProductBloc>()),
+        ],
+        child: BlocBuilder<ProfileBloc, ProfileState>(
+          builder: (context, state) {
+            return MaterialApp(
+              builder: (context, child) {
+                final mediaQuery = MediaQuery.of(context);
+                return MediaQuery(
+                  data: mediaQuery.copyWith(
+                    textScaler: TextScaler.noScaling,
+                    boldText: false,
+                  ),
+                  child: child!,
+                );
+              },
+              localizationsDelegates: [
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+              ],
+              supportedLocales: [Locale('fa', 'IR')],
+              locale: Locale('fa', 'IR'),
+              debugShowCheckedModeBanner: false,
+              theme: state.isDark ? AppTheme.darkTheme : AppTheme.lightTheme,
+              home: SplashScreen(),
+            );
+          },
+        ),
+      ),
     );
   }
 }
