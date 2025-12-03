72e4aefb9d5c799f3a23d5e0f12323d78d9c63fa
FarzinNs83 <farzinns83@gmail.com>
2025-12-03 15:00:01 +0330

    feat(auth): update dropdown widget

--- lib/feature/auth/presentation/widgets/drop_down_form.dart
+++ lib/feature/auth/presentation/widgets/drop_down_form.dart
@@ -1,14 +1,21 @@
-import 'package:flutter/material.dart';
 
-import 'package:goldooni/core/resources/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 import 'package:goldooni/core/utils/app_ext.dart';
 
 // ignore: must_be_immutable
 class DropDownForm extends StatefulWidget {
-  DropDownForm({super.key, required this.value, required this.items, required this.hint});
+  DropDownForm({
+    super.key,
+    required this.value,
+    required this.items,
+    required this.hint, required this.onChanged,
+  });
   String? value;
   final List<String> items;
   final String hint;
+  final ValueChanged<String?> onChanged;
 
   @override
   State<DropDownForm> createState() => _DropDownFormState();
@@ -17,58 +24,61 @@ class DropDownForm extends StatefulWidget {
 class _DropDownFormState extends State<DropDownForm> {
   @override
   Widget build(BuildContext context) {
+    final colors = context.theme.colorScheme;
+    final textTheme = context.textTheme;
     return Form(
       child: DropdownButtonFormField<String>(
-        menuMaxHeight: context.screenHeight * .4,
-        dropdownColor: AppColors.fourthColor,
+        menuMaxHeight: 0.4.sh,
+        dropdownColor: colors.surfaceContainerHighest,
         icon: Icon(
           Icons.keyboard_arrow_down_rounded,
-          color: AppColors.neturalColor8,
+          color: colors.onSurface,
         ),
         decoration: InputDecoration(
           filled: true,
-          fillColor: AppColors.teriaryColor.withValues(alpha: .5),
+          fillColor: colors.surfaceContainerHighest,
           hintText: widget.hint,
           enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(16),
+            borderRadius: BorderRadius.circular(16.r),
             borderSide: BorderSide.none,
           ),
           focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(16),
-            borderSide: BorderSide(color: AppColors.primaryColor, width: 1.5),
+            borderRadius: BorderRadius.circular(16.r),
+            borderSide: BorderSide(color: colors.primary, width: 1.5),
           ),
           errorBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(16),
+            borderRadius: BorderRadius.circular(16.r),
             borderSide: const BorderSide(color: Colors.red),
           ),
         ),
         validator: (value) =>
             value == null ? 'لطفاً تاریخ تولد را انتخاب کنید' : null,
-        style: const TextStyle(
-          fontSize: 15,
-          color: AppColors.neturalColor8,
-          fontWeight: FontWeight.w500,
+        style: textTheme.bodyMedium?.copyWith(
+          color: colors.onSurface,
+          fontWeight: FontWeight.w600,
         ),
-        borderRadius: BorderRadius.circular(16),
+        borderRadius: BorderRadius.circular(16.r),
         items: widget.items
             .map(
               (e) => DropdownMenuItem<String>(
                 value: e,
                 child: Text(
                   e,
-                  style: const TextStyle(
-                    color: AppColors.neturalColor8,
+                  style: textTheme.bodyMedium?.copyWith(
+                    color: colors.onSurface,
                     fontWeight: FontWeight.w500,
                   ),
                 ),
               ),
             )
             .toList(),
-        onChanged: (values) {
-          setState(() {
-            widget.value = values;
-          });
-        },
+        onChanged: widget.onChanged
+        // (values) {
+        //   setState(() {
+        //     widget.value = values;
+        //     log(values.toString());
+        //   });
+        // },
       ),
     );
   }
