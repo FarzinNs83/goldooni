5515b43ca53f7d8c2cca83bc511ae31f9c892615
FarzinNs83 <farzinns83@gmail.com>
2025-12-03 15:00:14 +0330

    refactor(splash): refine SplashState

--- lib/feature/splash/presentation/bloc/splash_state.dart
+++ lib/feature/splash/presentation/bloc/splash_state.dart
@@ -3,3 +3,7 @@ part of 'splash_bloc.dart';
 abstract class SplashState {}
 
 class SplashInitial extends SplashState {}
+class SplashAccessSuccess extends SplashState {}
+class SplashRefreshSuccess extends SplashState {}
+class SplashAccessFailure extends SplashState {}
+class SplashRefreshFailure extends SplashState {}
