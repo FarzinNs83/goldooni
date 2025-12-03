288ea1068177a5321317562428a0851024e9b8bf
FarzinNs83 <farzinns83@gmail.com>
2025-12-03 15:00:08 +0330

    refactor(home): improve HomeState

--- lib/feature/home/presentation/bloc/home_state.dart
+++ lib/feature/home/presentation/bloc/home_state.dart
@@ -3,3 +3,16 @@ part of 'home_bloc.dart';
 abstract class HomeState {}
 
 class HomeInitial extends HomeState {}
+
+class HomeAmazLoading extends HomeState {}
+class HomeCatsLoading extends HomeState {}
+class HomeNewLoading extends HomeState {}
+class HomeTopLoading extends HomeState {}
+
+
+class HomeSuccess extends HomeState {}
+
+class HomeFailed extends HomeState {
+  final Failure failure;
+  HomeFailed({required this.failure});
+}
