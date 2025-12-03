7f69313ce78e61a30c90f9f49ea0c6f585d45251
FarzinNs83 <farzinns83@gmail.com>
2025-12-03 14:59:59 +0330

    refactor(auth): update AuthBloc logic

--- lib/feature/auth/presentation/bloc/auth_bloc.dart
+++ lib/feature/auth/presentation/bloc/auth_bloc.dart
@@ -1,13 +1,102 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:fpdart/fpdart.dart';
+import 'dart:async';
 
-part 'auth_event.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:goldooni/feature/auth/domain/entities/send_sms_entity.dart';
+import 'package:goldooni/feature/auth/domain/usecases/register_form_usecase.dart';
+import 'package:goldooni/feature/auth/domain/usecases/send_sms_usecase.dart';
+import 'package:goldooni/feature/auth/domain/usecases/verify_otp_usecase.dart';
+
+import '../../../../core/errors/failure.dart';
+import '../../../../core/utils/app_ext.dart';
+import '../../domain/entities/register_form_entity.dart';
+import '../../domain/entities/verify_otp_entity.dart';
+
 part 'auth_state.dart';
 
-class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(AuthInitial()) {
-    on<AuthEvent>((event, emit) {
-      // TODO: Add event handling
+class AuthBloc extends Cubit<AuthState> {
+  final SendSmsUseCase sendSmsUseCase;
+  final VerifyOtpUseCase verifyOtpUseCase;
+  final RegisterFormUseCase registerFormUseCase;
+  Timer? _timer;
+  int _start = 120;
+  AuthBloc(this.sendSmsUseCase, this.verifyOtpUseCase, this.registerFormUseCase)
+    : super(AuthInitial());
+
+  Future<void> sendSms({required String phoneNum}) async {
+    emit(AuthLoading());
+    final res = await sendSmsUseCase(SendSmsParams(phoneNum: phoneNum));
+    res.fold((l) => emit(AuthError(l)), (r) {
+      emit(AuthSendSms(sendSmsEntity: r));
     });
+  }
+
+  Future<void> verifyOtp({
+    required String otp,
+    required String phoneNum,
+  }) async {
+    emit(AuthLoading());
+    final res = await verifyOtpUseCase(
+      VerifyOtpParams(otp: otp, phoneNum: phoneNum),
+    );
+    res.fold(
+      (l) => emit(AuthError(l)),
+      (r) => emit(AuthVerifyOtp(verifyOtpEntity: r)),
+    );
+  }
+
+  Future<void> register({
+    required String name,
+    required String address,
+    required String birthdate,
+    required String phoneNum,
+    required String codeMeli,
+    required String lat,
+    required String lon,
+    required String pw,
+  }) async {
+    emit(AuthLoading());
+    final res = await registerFormUseCase(
+      RegisterParams(
+        name: name,
+        address: address,
+        birthdate: birthdate,
+        phoneNum: phoneNum,
+        codeMeli: codeMeli,
+        lat: lat,
+        lon: lon,
+        pw: pw,
+      ),
+    );
+    res.fold(
+      (l) => emit(AuthError(l)),
+      (r) => emit(AuthRegister(registerFormEntity: r)),
+    );
+  }
+
+  void startTimer() {
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      if (_start > 0) {
+        _start--;
+        emit(AuthTimerTick(_start));
+      } else {
+        timer.cancel();
+        emit(AuthTimerFinished("زمان اعتبار کد به پایان رسید."));
+      }
+    });
+  }
+ void cancelTimer() {
+    _timer?.cancel();
+    _start = 120;
+ }
+  String formatTimer(int time) {
+    var duration = Duration(seconds: time);
+    String twoDigits(int n) => n.toString().padLeft(2, "0");
+    String twoDigitMinutes = twoDigits(
+      duration.inMinutes.remainder(60),
+    ).toPersianNumber();
+    String twoDigitSeconds = twoDigits(
+      duration.inSeconds.remainder(60),
+    ).toPersianNumber();
+    return "$twoDigitMinutes:$twoDigitSeconds";
   }
 }
