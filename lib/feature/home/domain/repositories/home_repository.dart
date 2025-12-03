874a2a67940d5d3e336d9997522d36c5a68ce703
FarzinNs83 <farzinns83@gmail.com>
2025-12-03 15:00:07 +0330

    refactor(home): update HomeRepository interface

--- lib/feature/home/domain/repositories/home_repository.dart
+++ lib/feature/home/domain/repositories/home_repository.dart
@@ -1,1 +1,14 @@
-abstract class HomeRepository {}
+import 'package:fpdart/fpdart.dart';
+import 'package:goldooni/core/errors/failure.dart';
+import 'package:goldooni/feature/home/domain/entities/amazing_products_entity.dart';
+import 'package:goldooni/feature/home/domain/entities/cats_entity.dart';
+import 'package:goldooni/feature/home/domain/entities/top_products_entity.dart';
+
+import '../entities/new_products_entity.dart';
+
+abstract class HomeRepository {
+  Future<Either<Failure,List<CatsEntity>>> getCats(int page);
+  Future<Either<Failure,List<AmazingProductsEntity>>> getAmazingProducts(int page);
+  Future<Either<Failure,List<NewProductsEntity>>> getNewProducts(int page);
+  Future<Either<Failure,List<TopProductsEntity>>> getTopProducts(int page);
+}
