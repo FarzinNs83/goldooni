51bd852058371f39c1a0342c8c327a5576998d4b
FarzinNs83 <farzinns83@gmail.com>
2026-01-03 23:31:41 +0330

    legacy packaging

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,7 +18,24 @@ android {
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
-
+    packagingOptions {
+        jniLibs {
+            useLegacyPackaging = true
+        }
+        resources {
+            excludes += setOf(
+                "META-INF/*.kotlin_module",
+                "META-INF/*.version",
+                "META-INF/AL2.0",
+                "META-INF/LGPL2.1",
+                "META-INF/LICENSE*",
+                "META-INF/NOTICE*",
+                "META-INF/DEPENDENCIES",
+                "META-INF/proguard/*",
+                "META-INF/gradle/incremental.annotation.processors"
+            )
+        }
+    }
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.goldooni"
